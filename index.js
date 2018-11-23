4326d09235a72116854b49fdfe4a69b15f139c89
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-23 18:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // Add your doToElementsInArray() function here:
+var evens = [0, 2, 4, 6, 8, 10];
+ 
+function doToEvens(callback) {
+  evens.forEach(callback);
+}
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
 
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+}
+ 
+var animals = ["dog", "cat", "squirrel"];
+ 
+doToElementsInArray(animals, changeCompletely);
+ 
+// log out animals -- pretty cool, right?
+console.log(animals);
